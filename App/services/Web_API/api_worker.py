2e7b63a74c559541fb049ce7e0cbf3abd5dc226f
A1exfin <alexfin16@gmail.com>
2025-09-04 21:08:24 +0700

    Начал писать ApiWorker

--- App/services/Web_API/api_worker.py
+++ App/services/Web_API/api_worker.py
@@ -1,7 +1,11 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Optional, Any
+import json
 
 import requests
-from PySide6.QtCore import QThread
+import urllib3
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+from PySide6.QtCore import QObject, Signal
 
 import Core
 from Core.settings import APIEndpoints
@@ -10,5 +14,47 @@ if TYPE_CHECKING:
     pass
 
 
-class APIWorker(QThread):
-    pass
+class APIWorker(QObject):
+    requestStarted = Signal()
+    requestFinished = Signal()
+    requestFailed = Signal()
+    requestProgress = Signal()
+
+    def __init__(self):
+        super().__init__()
+        self._session = requests.Session()
+        self._setup_session()
+
+    def _setup_session(self) -> None:
+        retry_strategy = Retry(
+            total=5,
+            backoff_factor=1.5,
+            status_forcelist=[429, 500, 502, 503, 504],
+        )
+
+        adapter = HTTPAdapter(max_retries=retry_strategy)
+
+        self._session.mount('http://', adapter)
+        self._session.mount('https://', adapter)
+
+        if Core.DEBUG:
+            self._session.verify = False
+            urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    def send_request(self, url: str, method: str, data: Optional[dict] = None):
+        try:
+            if data:
+                response = self._session.request(method, url, data=data)
+            else:
+                response = self._session.request(method, url)
+            response.raise_for_status()
+
+            try:
+                result = response.json()
+            except result.JSONDecodeError:
+                result = {'text': response.text}
+            self.requestFinished.emit(result)
+        except requests.exceptions.RequestException as e:
+            self.requestFailed.emit(f'Ошибка запроса: {str(e)}')
+        except Exception as e:
+            self.requestFailed.emit(f'Неизвестная ошибка: {str(e)}')
